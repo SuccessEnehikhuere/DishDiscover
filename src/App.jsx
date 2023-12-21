b9720f78c4320af016dbb8dd7829361822de2a4f
Success Enehikhuere <enehikhueresuccess@gmail.com>
2023-12-21 01:20:15 +0100

    feat: install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { HomeLayout, Landing, About, Newsletter, Error, Dish } from './pages'
 
-function App() {
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/Landing',
+    element: <Landing />,
+  },
+  {
+    path: '/About',
+    element: <About />,
+  },
+  {
+    path: '/Newsletter',
+    element: <Newsletter />,
+  },
+  {
+    path: '/Error',
+    element: <Error />,
+  },
+  {
+    path: '/Dish',
+    element: <Dish />,
+  },
+])
+
+
+
+
+
+
+const App = ()=>{
+
   return (
-    <main>
-      <h2>hello</h2>
-    </main>
+    <RouterProvider router={router}/>
   )
 }
 
